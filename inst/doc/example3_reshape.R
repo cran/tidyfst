5c80495a02c63e711e3279471d1bb718884bdfa1
Tian-Yuan Huang <huang.tian-yuan@qq.com>
2020-03-24 09:10:02 +0000

    version 0.9.3

--- inst/doc/example3_reshape.R
+++ inst/doc/example3_reshape.R
@@ -17,7 +17,7 @@ relig_income
 
 ## ----warning=FALSE------------------------------------------------------------
 relig_income %>% 
-  longer_dt("religion",gathered_name = "income",gathered_value = "count")
+  longer_dt("religion",name = "income",value = "count")
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  billboard
@@ -33,12 +33,12 @@ relig_income %>%
 #  
 #  # tidyfst way:
 #  billboard %>%
-#    longer_dt("wk",negate = TRUE,
-#              gathered_name = "week",
-#              gathered_value = "rank",
+#    longer_dt(-"wk",
+#              name = "week",
+#              value = "rank",
 #              na.rm = TRUE
 #              )
-#  # regex should select group_to_keep, negate could reverse that
+#  # regex could select the groups to keep, and minus could select the reverse
 
 ## -----------------------------------------------------------------------------
 ## data
@@ -50,9 +50,48 @@ fish_encounters %>%
 
 ## tidyfst way:
 fish_encounters %>% 
-  wider_dt(name_to_spread = "station",value_to_spread = "seen")
+  wider_dt(name = "station",value = "seen")
+
+# if no keeped groups are selected, use all except for name and value columns
 
 ## -----------------------------------------------------------------------------
 fish_encounters %>% 
-  wider_dt(name_to_spread = "station",value_to_spread = "seen",fill = 0)
+  wider_dt(name = "station",value = "seen",fill = 0)
+
+## -----------------------------------------------------------------------------
+family <- fread("family_id age_mother dob_child1 dob_child2 dob_child3 gender_child1 gender_child2 gender_child3
+1         30 1998-11-26 2000-01-29         NA             1             2            NA
+2         27 1996-06-22         NA         NA             2            NA            NA
+3         26 2002-07-11 2004-04-05 2007-09-02             2             2             1
+4         32 2004-10-10 2009-08-27 2012-07-21             1             1             1
+5         29 2000-12-05 2005-02-28         NA             2             1            NA")
+
+family
+
+## -----------------------------------------------------------------------------
+#     family_id age_mother  child        dob gender
+#         <int>      <int> <char>     <char> <char>
+#  1:         1         30 child1 1998-11-26      1
+#  2:         1         30 child2 2000-01-29      2
+#  3:         1         30 child3       <NA>   <NA>
+#  4:         2         27 child1 1996-06-22      2
+#  5:         2         27 child2       <NA>   <NA>
+#  6:         2         27 child3       <NA>   <NA>
+#  7:         3         26 child1 2002-07-11      2
+#  8:         3         26 child2 2004-04-05      2
+#  9:         3         26 child3 2007-09-02      1
+# 10:         4         32 child1 2004-10-10      1
+# 11:         4         32 child2 2009-08-27      1
+# 12:         4         32 child3 2012-07-21      1
+# 13:         5         29 child1 2000-12-05      2
+# 14:         5         29 child2 2005-02-28      1
+# 15:         5         29 child3       <NA>   <NA>
+
+## ----warning=FALSE------------------------------------------------------------
+family %>% 
+  longer_dt(1:2) %>% 
+  separate_dt("name",into = c("class","child")) %>% 
+  wider_dt(-"class|value",
+           name = "class",
+           value = "value")
 
